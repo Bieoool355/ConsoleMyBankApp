3304054ac700e6d5dbb6f924cef9f041f25be582
Gabriel Lúcio <bielmengao_@hotmail.com>
2021-10-15 22:10:58 -0300

    Código finalizado - MyBankApp

--- ConsoleMyBankApp/Program.cs
+++ ConsoleMyBankApp/Program.cs
@@ -6,7 +6,43 @@ namespace ConsoleMyBankApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cliente cliente = new Cliente("Paulo Jefferson Mendes Oliveira", "000.000.000-00", "Desenvolvedor");
+
+            ContaCorrente conta = new ContaCorrente(cliente, 01, 0001);
+
+            Console.WriteLine("Conta Corrente Criada");
+            Console.WriteLine("Titular: {0}", conta.getTitular().getNome());
+            Console.WriteLine("Agência: {0}", conta.getAgencia());
+            Console.WriteLine("Conta: {0}", conta.getConta());
+
+            Console.WriteLine("Parabéns! Seu saldo é de R${0}, obrigado por criar sua conta!", conta.getSaldo());
+
+            double valorDeposito = 200;
+            conta.Depositar(valorDeposito);
+
+            Console.WriteLine("Seu saldo é de R${0}", conta.getSaldo());
+
+            double valorSaque = 100;
+            conta.Sacar(valorSaque);
+
+            Console.WriteLine("Seu saldo é de R${0}", conta.getSaldo());
+
+            Cliente clienteSilvia = new Cliente("Silvia Mendes Oliveira", "000.000.000-00", "Administradora");
+
+            ContaCorrente contaSilvia = new ContaCorrente(clienteSilvia, 01, 0002);
+
+            double valorPix = 100;
+            conta.Pix(valorPix, contaSilvia);
+
+            Console.WriteLine("Seu saldo é de R${0}", conta.getSaldo());
+            Console.WriteLine("Seu saldo é de R${0}", contaSilvia.getSaldo());
+
+            ContaPoupança contaPoupança = new ContaPoupança(cliente, 01, 0003);
+
+            Console.WriteLine("Conta Poupança criada!");
+            Console.WriteLine("Titular: {0}", contaPoupança.getTitular().getNome());
+            Console.WriteLine("Agencia: {0}", contaPoupança.getAgencia());
+            Console.WriteLine("Conta: {0}", contaPoupança.getConta());
         }
     }
 }
